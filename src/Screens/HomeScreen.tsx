0674a0efddc0b545f1faceac29a3d95af6aaf1bf
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-08-02 23:36:49 +0300

    prettier'd

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import HomeContent from '../Content/HomeContent';
 
 export default class HomeScreen extends Component {
-  private contentStyle: React.CSSProperties = {
+private contentStyle: React.CSSProperties = {
     height: window.innerHeight,
     color: '#fff',
     background: '#364d79',
   };
+
   render() {
     return (
         <div style={this.contentStyle}>
