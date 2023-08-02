ac85ce78605117bc8d59bef5af6592d8bd5beea1
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-08-03 00:13:57 +0300

    new style of height

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import HomeContent from '../Content/HomeContent';
 
 export default class HomeScreen extends Component {
-private contentStyle: React.CSSProperties = {
-    height: window.innerHeight,
+  private contentStyle: React.CSSProperties = {
+    height: window.innerHeight - 500 ,
+    marginTop: '80px',
     color: '#fff',
     background: '#364d79',
   };
 
-  render() {
+  render(): React.JSX.Element {
     return (
-        <div style={this.contentStyle}>
-            <HomeContent />
-        </div>
+      <div style={this.contentStyle}>
+        <HomeContent/>
+      </div>
     );
   }
 }
