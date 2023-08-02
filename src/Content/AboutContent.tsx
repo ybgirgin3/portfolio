517fdfe1a240fb0138224ecaf3eff52d9af1b791
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-08-02 23:36:27 +0300

    Multiple Accordion exports created

---
+++ src/Content/AboutContent.tsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+export class AboutMeContent extends Component {
+  render() {
+    return (
+      <div>
+        AboutContent
+      </div>
+    );
+  }
+}
+
+export class ExperiencesContent extends Component {
+  render() {
+    return (
+      <div>Experiences</div>
+    )
+  }
+}
+
+
+export class ProjectsContent extends Component {
+  render() {
+    return (
+      <div>Experiences</div>
+    )
+  }
+}
+
