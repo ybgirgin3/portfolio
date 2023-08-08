dbf194fc41ba0959bfeaa0e8d5dcc8c92b85de4a
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2023-08-09 00:32:39 +0300

    card factory is in a middle inner container and margined via this container

--- src/Screens/ProjectsScreen.tsx
+++ src/Screens/ProjectsScreen.tsx
@@ -7,10 +7,14 @@ import { CardFactory } from '../Components/CardFactory';
 // styling
 export default class ProjectsScreen extends Component {
   container: React.CSSProperties = {
-    // backgroundColor: 'lightgrey',
+    backgroundColor: '#364d79',
     // textAlign: 'center',
   };
   rowStyle: React.CSSProperties = {
+    // marginTop: '20px',
+  };
+
+  innerContainerStyle: React.CSSProperties = {
     marginTop: '20px',
   };
 
@@ -20,7 +24,9 @@ export default class ProjectsScreen extends Component {
         {/* first row */}
         <Row style={this.rowStyle}>
           {Projects.map((project, index) => (
-            <CardFactory key={index} {...project} />
+            <div style={this.innerContainerStyle}>
+              <CardFactory key={index} {...project} />
+            </div>
           ))}
         </Row>
       </div>
